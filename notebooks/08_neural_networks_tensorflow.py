547154b8ff0f7ed8110631766aa33bac23841427
Jessica de Aguila <doulus.jefis@gmail.com>
2026-03-08 21:25:29 -0600

    Add TensorFlow model definition and compilation

--- notebooks/08_neural_networks_tensorflow.py
+++ notebooks/08_neural_networks_tensorflow.py
@@ -1,1 +1,12 @@
+import tensorflow as tf
+from tensorflow import keras
 
+model = keras.Sequential([
+    keras.layers.Dense(10,activation="relu"),
+    keras.layers.Dense(1)
+])
+
+model.compile(
+optimizer="adam",
+loss="mse"
+)
