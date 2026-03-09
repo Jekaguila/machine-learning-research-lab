f7a704ab06b5bcd398f2bbba3853fcb2e0ea4407
Jessica de Aguila <doulus.jefis@gmail.com>
2026-03-08 21:24:01 -0600

    Add RandomForestRegressor for advanced regression

--- notebooks/04_advanced_regression.py
+++ notebooks/04_advanced_regression.py
@@ -1,1 +1,11 @@
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.datasets import load_boston
 
+data = load_boston()
+
+X = data.data
+y = data.target
+
+model = RandomForestRegressor()
+
+model.fit(X,y)
