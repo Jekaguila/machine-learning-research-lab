d273865183fafeb419461e7361594e3f763311cd
Jessica de Aguila <doulus.jefis@gmail.com>
2026-03-08 21:24:55 -0600

    Add KMeans clustering to iris dataset analysis

--- notebooks/07_clustering_unsupervised_learning.py
+++ notebooks/07_clustering_unsupervised_learning.py
@@ -1,1 +1,12 @@
+from sklearn.cluster import KMeans
+import seaborn as sns
 
+data = sns.load_dataset("iris")
+
+X = data.drop("species",axis=1)
+
+kmeans = KMeans(n_clusters=3)
+
+kmeans.fit(X)
+
+kmeans.labels_
