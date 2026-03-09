8bb8a9db1fd7c397c053c2b615ff7644d414b5d6
Jessica de Aguila <doulus.jefis@gmail.com>
2026-03-08 21:24:18 -0600

    Add RandomForestClassifier model for iris dataset

--- notebooks/05_classification_models.py
+++ notebooks/05_classification_models.py
@@ -1,1 +1,11 @@
+from sklearn.ensemble import RandomForestClassifier
+from sklearn.datasets import load_iris
 
+data = load_iris()
+
+X = data.data
+y = data.target
+
+model = RandomForestClassifier()
+
+model.fit(X,y)
