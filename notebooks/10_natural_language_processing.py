22707e51c23d4322c856c470b5a72df5ab034c98
Jessica de Aguila <doulus.jefis@gmail.com>
2026-03-08 21:26:09 -0600

    Add CountVectorizer example for text processing

--- notebooks/10_natural_language_processing.py
+++ notebooks/10_natural_language_processing.py
@@ -1,1 +1,13 @@
+import nltk
+from sklearn.feature_extraction.text import CountVectorizer
 
+texts = [
+"machine learning is powerful",
+"data science is interesting"
+]
+
+vectorizer = CountVectorizer()
+
+X = vectorizer.fit_transform(texts)
+
+print(X.toarray())
